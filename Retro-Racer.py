97c30be31687264fc0ca3efb21f13a24872236c2
anayatc@gmail.com <anayatc@sky.com>
2017-06-11 18:34:02 +0100

    defined fps and quit

--- Retro-Racer.py
+++ Retro-Racer.py
@@ -12,3 +12,11 @@ while not crashed:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             crashed = True
+
+        print(event)
+
+    pygame.display.update()
+    clock.tick(60)
+
+pygame.quit()
+quit()
